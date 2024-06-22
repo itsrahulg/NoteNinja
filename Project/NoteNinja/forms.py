4bf987831dcdb537ee32129d9f8e45c3389f4edb
itsrahulg <rahulstark13@gmail.com>
2024-06-22 07:49:46 +0530

    finished login and register UI

--- Project/NoteNinja/forms.py
+++ Project/NoteNinja/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -9,6 +10,56 @@ from django.forms.widgets import PasswordInput, TextInput
 
 class CreateUserForm(UserCreationForm):
 
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({
+            'required' : '',
+            'name' : 'username',
+            'id': 'username',
+            'type':'text',
+            'class' : 'form-control',
+            'placeholder' :'Enter username',
+            'maxlength' : '20',
+            'minlength' : '5'
+        })
+
+
+        self.fields["email"].widget.attrs.update({
+            'required' : '',
+            'name' : 'email',
+            'id': 'email',
+            'type':'email',
+            'class' : 'form-control',
+            'placeholder' :'Email-id',
+            'maxlength' : '30',
+            'minlength' : '5'
+        })
+
+
+        self.fields["password1"].widget.attrs.update({
+            'required' : '',
+            'name' : 'password1',
+            'id': 'password1',
+            'type':'password',
+            'class' : 'form-control',
+            'placeholder' :'Password',
+            'maxlength' : '16',
+            'minlength' : '8'
+        })
+
+
+        self.fields["password2"].widget.attrs.update({
+            'required' : '',
+            'name' : 'password2',
+            'id': 'password2',
+            'type':'password',
+            'class' : 'form-control',
+            'placeholder' :'Confirm Password',
+            'maxlength' : '16',
+            'minlength' : '8'
+        })
+
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -16,6 +67,30 @@ class CreateUserForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({
+            'required' : '',
+            'name' : 'username',
+            'id': 'username',
+            'type':'text',
+            'class' : 'form-control',
+            'placeholder' :'Enter username',
+            'maxlength' : '20',
+            'minlength' : '5'
+        })
+
+        self.fields["password"].widget.attrs.update({
+            'required' : '',
+            'name' : 'password',
+            'id': 'password',
+            'type':'password',
+            'class' : 'form-control',
+            'placeholder' :'Password',
+            'maxlength' : '16',
+            'minlength' : '8'
+        })
 
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
